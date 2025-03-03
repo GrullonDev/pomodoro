172b54f5480b28bae94a84c38043e362db75aff8
GrullonDev <prosystem155@gmail.com>
2025-03-03 14:48:26 -0600

    Elimina código comentado en main.dart y añade nuevas dependencias para notificaciones y servicios en segundo plano

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,4 @@ import 'package:pomodoro/utils/app.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
-
-  // if (Platform.isMacOS) {
-  //   final windowManager = WindowManager.instance;
-  //   windowManager.setMinimumSize(const Size(1650, 1600));
-  //   windowManager.setMaximumSize(const Size(1650, 1600));
-  // }
 }
